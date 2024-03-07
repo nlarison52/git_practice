d29e65a04a8e1913f80f459139ffd3d82360797e
Nolan Larison <nlarison@Nolans-MBP.attlocal.net>
2024-03-07 15:20:50 -0500

    Added a crappy prime factorization algorithm

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
 using namespace std;
+int primes[] = {1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
 int fib(int iterations); //returns the fibonacci number for the given index
 
-int main(){
+void primeFactorization(int number, vector<int> &result);
 
+
+int main(){
+    vector<int> factorPowers;
+    int num = 9;
+
+    primeFactorization(num, factorPowers);
+
+    int len = sizeof(factorPowers) / sizeof(factorPowers[0]);
+
+    for (int i = 0; i < 11; i++){
+        cout << factorPowers[i] << " : " << primes[i] << endl;
+    }
 
     
 
@@ -25,3 +38,23 @@ int fib(int iterations){
     }
     return x;
 }
+
+
+void primeFactorization(int number, vector<int> &result){
+    
+    int primeSize = sizeof(primes) / sizeof(primes[0]);
+    result.push_back(1);
+
+    int powerCount;
+    while (number != 1){
+        for (int i = 1; i < primeSize; i++){
+            powerCount = 0;
+            while (!(number % primes[i])){
+                    powerCount ++;
+                    number /= primes[i];
+            }
+            result.push_back(powerCount);
+        }
+    }
+}
+
